c557d80c3259b180317a856e87b87cc8e29f456e
Doug Phung <phung.doug@gmail.com>
2014-12-23 18:07:25 -0800

    changed url again. please deploy

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://dougphuhg.com',
+        url: 'http://floofyblog.azurewebsites.net',
         mail: {
             transport: 'SMTP',
             options: {
